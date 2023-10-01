7f00ba2a7248f422836fc9d5cc598edb6be8ffd4
Maks <98954111+MunMaks@users.noreply.github.com>
2023-10-01 12:25:14 +0200

    Delete show_cube.c

--- show_cube.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void display_info(int nb, int cube){ printf("Le cube du nombre %d est le nombre %d.\n", nb, cube); }
-
-int calculate_cube(int nb){ return nb * nb * nb; }
-
-void show_cube(int n){
-    int cube = calculate_cube(n);
-    display_info(n, cube);
-}
