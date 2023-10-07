50e8f764d43d40b3436b2a006520cb4e38a1ef14
Maks <98954111+MunMaks@users.noreply.github.com>
2023-10-07 22:53:43 +0200

    Create retourne_chaine.c

--- String_in_C/retourne_chaine.c
+++ String_in_C/retourne_chaine.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 // without using strlen() from #include <string.h> 
-int len (char word[]){  // returns lenght of the string (array of character)
+int len (char word[]){  // returns length of the string (array of character)
     int i = 0;
     while (word[i]) { ++i; }
     return i;
 }
 
 void retourne_chaine(char s[]){
-    int lenght = len(s);  // The lenght of our string
-    char miroir[lenght];  // Miroir with the same lenght than our string 's'
+    int length = len(s);  // The length of our string
+    char miroir[length];  // Miroir with the same lenght than our string 's'
     
     // miroir = l'inverse de notre chaine de caracteres
-    for (int i = 0; i < lenght; ++i){ miroir[i] = s[lenght - i - 1]; }   // From the end to the 0 index copy of s
+    for (int i = 0; i < length; ++i){ miroir[i] = s[length - i - 1]; }   // From the end to the 0 index copy of s
     // la mis a jour de notre chaine de caracteres initiale
-    for (int i = 0; i < lenght; ++i){ s[i] = miroir[i]; }                // Modify original string by created 'miroir'
+    for (int i = 0; i < length; ++i){ s[i] = miroir[i]; }                // Modify original string by created 'miroir'
 }
