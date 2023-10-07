e014413aeb9a9fbfddcf58c00dc14babfb2fab3e
Maks <98954111+MunMaks@users.noreply.github.com>
2023-10-07 22:52:56 +0200

    Create palindrome.c

--- String_in_C/palindrome.c
+++ String_in_C/palindrome.c
@@ -6,8 +6,8 @@ int string_length(char* s){
 }
 
 int est_palindrome(char s[]){
-    int len = string_length(s);  // the lenght of string
-    int half = len / 2;          // half of the lenght
+    int len = string_length(s);  // the length of string
+    int half = len / 2;          // half of the length
 
     for (int i = 0; i < half; ++i){
         if (s[i] != s[len-i-1]) { return 0; }  // check if (first, len-1), (second, len-2) etc
